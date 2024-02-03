25f9ce9cb8a7fa5a3f08d7dfde09ace2f08b8cf5
Duncan <MacKinnon>
2024-02-03 17:05:59 +0000

    added an edit.ejs class

--- routes/articles.js
+++ routes/articles.js
@@ -20,6 +20,14 @@ router.post("/", (req, res) => {
 
 });
 
+router.put("/", (req, res) => {
+
+  const articles = [
+    {title: req.body["title"], createdAt:new Date(), description: req.body["description"]}
+  ];
+  res.render("articles/index.ejs", {articles: articles})
+
+});
 
 
 // Export the router as the default export
